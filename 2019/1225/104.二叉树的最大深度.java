c004893c5b1e5940ad00ec4b8b311d360650de8e
Shawley <shawley.xiao@foxmail.com>
2019-12-25 17:52:10 +0800

    104, 122

---
+++ 2019/1225/104.二叉树的最大深度.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=java
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left_depth = maxDepth(root.left);
+        int right_depth = maxDepth(root.right);
+        return Math.max(left_depth, right_depth) + 1;
+    }
+}
+// @lc code=end
+
