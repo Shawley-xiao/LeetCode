ffdc7753085e2fe978ddcb4cd170ba23ca4605e8
Shawley <shawley.xiao@foxmail.com>
2020-03-03 15:40:10 +0800

    279

---
+++ Java/279.完全平方数.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=279 lang=java
+ *
+ * [279] 完全平方数
+ */
+
+// @lc code=start
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            int j = 1;
+            while (i - j * j >= 0) {
+                min = Math.min(min, dp[i - j * j] + 1);
+                ++j;
+            }
+            dp[i] = min;
+        }
+        return dp[n];
+    }
+}
+// @lc code=end
+
